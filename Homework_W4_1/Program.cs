b9e6c8207db709a80e2b8295d5085e8538fc3a2c
norman <normanjuhkental@gmail.com>
2025-03-24 17:58:10 +0200

    e

--- Homework_W4_1/Program.cs
+++ Homework_W4_1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Loop
+{
+
+
+    class ForLoop
+    {
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter amount of money:  ");      //kysime sisestust
+            double raha = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter interest rate:  ");
+            double intress = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter amount of years:  ");
+            int aastad = Int32.Parse(Console.ReadLine());
+
+            double d = FindCompoundInterest(raha, intress, aastad);
+            Console.WriteLine("Resulting money: " + d.ToString());
+
+        }
+
+
+
+
+
+
+        public static double FindCompoundInterest(double initial, double interest, int years)
+        {
+            for (int i = 1; i <= years; i++)
+            {
+                initial = initial * (1 + (interest / 1));
+                
+            }
+
+
+
+            return initial;
+        }
+    }
+}
---
+++ Homework_W4_2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Loop
+{
+
+
+    class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            int reqLen = 11;
+            
+            Luger();
+
+        }
+
+
+
+
+
+
+        public static double method()
+        {
+            return 0;
+        }
+
+        static void Luger()
+        {
+            const string path1 = "koodid1.txt";     //defineerin filepathid
+            const string path2 = "koodid2.txt";
+            StreamReader reader1 = new StreamReader(path1);     //loon 2 streamreaderit
+            StreamReader reader2 = new StreamReader(path2);
+
+            //loon koodi alguste listi ja loen failist
+            List<string> starts = new List<string>();
+
+
+            string line = reader1.ReadLine();
+
+            //chekin et rida pole tyhi
+            while (line != null)
+            {
+                //Console.WriteLine(line);
+                starts.Add(line);           //debugi jaoks
+                line = reader1.ReadLine();
+            }
+            reader1.Close();
+
+
+
+            //loon koodi l6ppude listi ja loen failist
+            List<string> ends = new List<string>();
+
+
+            string line2 = reader2.ReadLine();
+
+            //chekin et rida pole tyhi
+            while (line2 != null)
+            {
+                //Console.WriteLine(line2); //debugi jaoks
+                ends.Add(line2);
+                line2 = reader2.ReadLine();
+            }
+            reader2.Close();
+
+
+
+
+            //uus list vastuste jaoks
+            List<string> vastused = new List<string>();
+            //otsin vastavad paarid
+            for (int i = 0; i < starts.Count; i++)
+            {
+                for (int o = 0; o < ends.Count; o++)
+                {
+                    if ((starts[i] + ends[o]).Length == 11)
+                    {
+                        vastused.Add(starts[i] + ends[o]);
+                    }
+                }
+            }
+
+            //prindin vastused
+            Console.WriteLine("Vastused");
+            foreach (string item in vastused)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        
+        
+    }
+}
+
+   
+        
+
+
